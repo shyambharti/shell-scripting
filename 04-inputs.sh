ab555aab057404ee3b5bece60952c757d8b8c111
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-11 21:37:36 -0800

    Variable command

---
+++ 04-inputs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter your Name: " name
+echo "Your Name = $name"
+
+# Special Variables
+# $0-$n , $* / $@, $#
+echo Scipt Name = $0
+echo First Argument = $1
+echo All Arguments = $*
+echo Number of Arguments = $#
