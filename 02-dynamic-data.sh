250123f501bca3e7aec5919c07d71ed5f460afa2
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-11 21:18:56 -0800

    dynamic data operation

--- 02-dynamic-data.sh
+++ 02-dynamic-data.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
-a=10
-b=20
+echo Hello World
 
-ADD=$(($a + $b))
+## Color Codes
+# Red         31
+# Green       32
+# yellow      33
+# Blue        34
+# Magenta     35
+# Cyan        36
 
-echo "sum $ADD"
+# Syntax : echo -e "\e[COLmHello\e[0m"
+# -e option is to enable esc seq, without -e colors will not work
+# "" Double Quotes are mandatory for colors to work, otherwise it will not work. Optionally we can use single quotes also, But preferred way is Double quot4es
+# \e[COLm -> this to enable the color, COL is one of the color code
+# \e[0m -> this is to disable the color
+
+echo -e "\e[31mText\e[0m in Red Color"
+echo "One more line"
+
+echo -e "Line1\n\nLine2"
+
+echo -e "Word1\t\tWord2"
