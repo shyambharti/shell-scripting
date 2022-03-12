eff8771c37f2869c81c2c6875ace52cc18e78f06
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-12 11:44:42 -0800

    Operator Numberic

--- 08-operatorNumberic.sh
+++ 08-operatorNumberic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "enter the String value for ==" z
 read -p "enter the String value for ==" y
-if [ $z -eq $y ]
+if [ $z=2 || $y=3 ]
 then
   echo "ok"
   else
@@ -10,8 +10,9 @@ fi
 
 echo "End"
 
-read -p "enter the String value for ==" y
-if [ -f y ]
+read -p "enter the String value for ==" file1
+read -p "enter the String value for ==" file2
+if [ -f $file1 -ef $file2 ]
 then
   echo "ok"
   else
