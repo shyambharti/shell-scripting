b80c933d27eae9d9566d2cc4ebdd38465ac8eadd
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-12 09:23:46 -0800

    common practics for shell-scripting

--- 06-common-practics.sh
+++ 06-common-practics.sh
@@ -1,5 +1,5 @@
 #!/bin/bhash
-Special variables are $0-$n, $*, $@, $#.
+#Special variables are $0-$n, $*, $@, $#.
 $0 - Script Name
 $1 - $n - Arguments parsed in the order.
  $* , $@ - All arguments
