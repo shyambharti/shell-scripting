2ec6ff1fab6620193da29c131b24feb79866ec0b
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-12 13:16:47 -0800

    Frondend Shell Script

--- roboshop/component/frondend.sh
+++ roboshop/component/frondend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
- #sudo yum install nginx -y
- #sudo systemctl enable nginx
- #sudo systemctl start nginx
+ yum install nginx -y
+ systemctl enable nginx
+ systemctl start nginx
 
- #sudo curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
- #sudo cd /usr/share/nginx/html
- #sudo rm -rf *
- #sudo unzip /tmp/frontend.zip
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+ cd /usr/share/nginx/html
+ rm -rf *
+ unzip /tmp/frontend.zip
  if [ "$?" -eq 0 ]
   then
     echo -e "\e[32m*Nginx Webserver restarted Successfully.   \e[0m"
@@ -20,28 +20,28 @@
   else
     echo -e "\e[31m*install nginx Not Successful.   \e[0m"
  fi
- sudo mv static/* .
+mv static/* .
  if [ "$?" -eq 0 ]
   then
     echo -e "\e[32m*Nginx Webserver restarted Successfully.   \e[0m"
   else
     echo -e "\e[31m*install nginx Not Successful.   \e[0m"
  fi
- sudo rm -rf frontend-main README.md
+rm -rf frontend-main README.md
  if [ "$?" -eq 0 ]
   then
     echo -e "\e[32m*Nginx Webserver restarted Successfully.   \e[0m"
   else
     echo -e "\e[31m*install nginx Not Successful.   \e[0m"
  fi
- sudo mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
  if [ "$?" -eq 0 ]
   then
     echo -e "\e[32m*Nginx Webserver restarted Successfully.   \e[0m"
   else
     echo -e "\e[31m*install nginx Not Successful.   \e[0m"
  fi
- sudo systemctl restart nginx
+systemctl restart nginx
  if [ "$?" -eq 0 ]
   then
     echo -e "\e[32m*Nginx Webserver restarted Successfully.   \e[0m"
