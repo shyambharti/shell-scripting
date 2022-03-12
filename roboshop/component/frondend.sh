67cfaeb33f2b5d0a07045ab20283e72198019a2f
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-12 12:54:27 -0800

    Frondend Shell Script

--- roboshop/component/frondend.sh
+++ roboshop/component/frondend.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
- sudo yum install nginx -y
- sudo systemctl enable nginx
- sudo systemctl start nginx
+ #sudo yum install nginx -y
+ #sudo systemctl enable nginx
+ #sudo systemctl start nginx
 
- # curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
- # cd /usr/share/nginx/html
- # rm -rf *
- # unzip /tmp/frontend.zip
- # mv frontend-main/* .
- # mv static/* .
- # rm -rf frontend-main README.md
- # mv localhost.conf /etc/nginx/default.d/roboshop.conf
- # systemctl restart nginx
+ sudo curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+ sudo cd /usr/share/nginx/html
+ sudo rm -rf *
+ sudo unzip /tmp/frontend.zip
+ sudo mv frontend-main/* .
+ sudo mv static/* .
+ sudo rm -rf frontend-main README.md
+ sudo mv localhost.conf /etc/nginx/default.d/roboshop.conf
+ sudo systemctl restart nginx
