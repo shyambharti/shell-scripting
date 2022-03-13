80bf9ea5e905aa964ebe7b26c337e4a18f86ba17
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-12 20:42:27 -0800

    updated catalogue.sh

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
- curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
- yum install nodejs gcc-c++ -y
+ #curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
+ #yum install nodejs gcc-c++ -y
 
-  useradd roboshop
+  #useradd roboshop
 
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+$ cd /home/roboshop
+$ unzip /tmp/catalogue.zip
+$ mv catalogue-main catalogue
+$ cd /home/roboshop/catalogue
+$ npm install
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
