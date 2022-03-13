d9c739fe8744759c24940cd4cd94169fc43fb221
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-12 20:48:19 -0800

    updated catalogue.sh

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -5,12 +5,12 @@
 
   #useradd roboshop
 
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
