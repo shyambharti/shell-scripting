624d42326f631e2e4001e2604f63025d395ac8d0
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-16 21:41:30 -0700

    frontend and mongodb automated

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -10,28 +10,28 @@ Print "Enable nginx Webserver -START"
  systemctl enable nginx &>>$LOG_FILE
 StatCheck $?
 
-print "Start nginx Webserver -START "
+Print "Start nginx Webserver -START "
 systemctl start nginx &>>$LOG_FILE
 StatCheck $?
 
-print "Start Downloading Frontend Application from git -START "
+Print "Start Downloading Frontend Application from git -START "
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 StatCheck $?
 
-print "Deploy in Nginx Default Location. -START"
+Print "Deploy in Nginx Default Location. -START"
  rm -rf /usr/share/nginx/html/*\
  StatCheck $?
 
 cd /usr/share/nginx/html/
 
-print "unzip the package"
+Print "unzip the package"
 unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
 StatCheck $?
 
-print "configuration of roboshop.config"
+Print "configuration of roboshop.config"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 StatCheck $?
 
-print "Restart nginx Server"
+Print "Restart nginx Server"
 systemctl restart nginx && systemctl enable nginx &>>$LOG_FILE
 StatCheck $?
--- roboshop/component/mongodb.sh
+++ roboshop/component/mongodb.sh
@@ -2,32 +2,32 @@
 
 source component/common.sh
 
-print "Setup MongoDB repos. -START"
+Print "Setup MongoDB repos. -START"
 curl -s -o /etc/yum.repos.d/mongodb.repo "https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo" &>>$LOG_FILE
 StatCheck $?
 
-print "Install mongodb "
+Print "Install mongodb "
  yum install -y mongodb-org &>>$LOG_FILE
 StatCheck $?
 
-print "conf change in mongod.conf "
+Print "conf change in mongod.conf "
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>$LOG_FILE
 StatCheck $?
 
-print "Enable mongodb "
+Print "Enable mongodb "
  systemctl enable mongod && systemctl start mongod &>>$LOG_FILE
 StatCheck $?
 
 
-print "Downloading mongodb schema from git"
+Print "Downloading mongodb schema from git"
  curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
 StatCheck $?
 
 
-print "archive mongodb"
+Print "archive mongodb"
  cd /tmp && unzip -o mongodb.zip &>>$LOG_FILE
  StatCheck $?
 
-print "login in mongodb"
+Print "login in mongodb"
 cd mongodb-main && mongo < catalogue.js && mongo < users.js &>>$LOG_FILE
 StatCheck $?
