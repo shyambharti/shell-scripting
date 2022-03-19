0a3c9ef389d629198b3d2dfb014345a9b05320b5
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-18 22:48:08 -0700

    ec2-instance-creation

--- create-ec2-instance.sh
+++ create-ec2-instance.sh
@@ -6,5 +6,5 @@ security_id=$(aws ec2 describe-security-groups --filters Name=group-name,Values=
 aws ec2 run-instances \
     --image-id ami-0bb6af715826253bf \
     --instance-type t2.micro \
-    --security-group-ids "${security_id}" \
+    --security-group-ids ${security_id} \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]'
