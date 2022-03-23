04dd82e82a8ed497a81915a30e2cfba5ccba218e
Shyam Bharti <shyam.bharti1@ibm.com>
2022-03-22 22:45:22 -0700

    public and private hosted zone script.

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -37,7 +37,7 @@ chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 StatCheck $?
 
 Print "systemd setting"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+sed -i -e 's/MONGO_DNSNAME/mongodb.ayushyam.internal/'
 /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service
 /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 StatCheck $?
